a1b84f3c7d054c0b424def6b01cf64b132c56daf
PolicarpoYU <poli@atomlig.com.br>
2024-07-26 21:09:53 -0300

    voltando bb
    con bb

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='ulianovorbit',
-    version='1.1.0',
-    description='Library for handling Ulianov Orbit functions',
+    name='ulianovellipse',
+    version='1.4.1',
+    description='Library for handling Ulianov elliptical functions',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Policarpo Yoshin Ulianov',
     author_email='poliyu77@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'numpy','ulianovellipse'
+        'numpy',
     ],
     setup_requires=[
-        'numpy','ulianovellipse'
+        'numpy',
     ],
 )
--- ulianovorbit/__init__.py
+++
@@ -1,5 +0,0 @@
-# ulianovellipse/__init__.py
-
-from .ulianoveorbit import ou
-
-__version__ = '1.1.0'
