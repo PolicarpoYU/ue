fc095d7744cd7e5d8d0f7aed945aaab71600f170
PolicarpoYU <poli@atomlig.com.br>
2024-07-28 00:11:57 -0300

    versao 1.6.1
    versao 1.6.1
    inclui imagen na pasta images

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ulianovellipse',
-    version='1.5.1',
+    version='1.6.1',
     description='Library for handling Ulianov elliptical functions',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -19,4 +19,9 @@ setup(
     setup_requires=[
         'numpy',
     ],
+    package_data={
+        # Inclua todos os arquivos .png na pasta images e na pasta examples
+        '': ['images/*.png', 'examples/*.py'],
+    },
+    include_package_data=True,
 )
